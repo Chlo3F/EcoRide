a2f6e131c6896f3c3f0478a3644794c6a2a1028a
Chloe Falcony <chloe.falcony@gmail.com>
2025-05-30 15:51:06 +0200

    prêt pour commencer prod

--- test_mongo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use MongoDB\Client;
-
-$client = new Client("mongodb://Jose:Eco2025@localhost:27017/?authSource=admin");
-
-echo "✅ Connexion OK via MongoDB\Client\n";
