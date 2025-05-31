e763fc5ed51cc13358b6a1d1cb50d180f9604754
Chloe Falcony <chloe.falcony@gmail.com>
2025-05-31 02:30:01 +0200

    Fix env loading for Platform.sh prod

--- public/index.php
+++ public/index.php
@@ -5,8 +5,14 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    // Forcer APP_ENV en prod si non défini (pour éviter la lecture de .env)
     if (!isset($context['APP_ENV'])) {
         $context['APP_ENV'] = 'prod';
     }
-    return new Kernel($context['APP_ENV'], (bool) ($context['APP_DEBUG'] ?? false));
+    // APP_DEBUG false par défaut si non défini
+    if (!isset($context['APP_DEBUG'])) {
+        $context['APP_DEBUG'] = false;
+    }
+
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
