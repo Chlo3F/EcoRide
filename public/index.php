94acb93f9deb9044a4b2a743f660fe68ecba1092
Chloe Falcony <chloe.falcony@gmail.com>
2025-05-31 02:28:19 +0200

    Préparation déploiement sur Platform.sh

--- public/index.php
+++ public/index.php
@@ -5,5 +5,8 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    if (!isset($context['APP_ENV'])) {
+        $context['APP_ENV'] = 'prod';
+    }
+    return new Kernel($context['APP_ENV'], (bool) ($context['APP_DEBUG'] ?? false));
 };
